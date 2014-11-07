c948fa2c87079946a608e6a93201376813089cb2
NgoSon <escitgroup@gmail.com>
2014-11-07 20:26:34 +0700

    Thay doi test.txt thành index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+<head>
+	<title>L&#7883;ch công tác tu&#7847;n</title>
+</head>
+
+<body>
+
+<?php
+
+print "L&#7883;ch công tác tu&#7847;n!"
+
+?>
+
+</body>
+</html>
